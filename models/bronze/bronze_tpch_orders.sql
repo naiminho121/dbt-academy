93340b2ac21b52ad7e35933983b11858f349cfb7
Naim Ares Alonso <naim.alonso@theinformationlab.es>
2026-06-16 08:20:25 +0000

    Day1

---
+++ models/bronze/bronze_tpch_orders.sql
@@ -0,0 +1,11 @@
+SELECT
+    o_orderkey                          AS order_id,
+    o_custkey                           AS customer_id,
+    o_orderstatus                       AS order_status,
+    o_totalprice                        AS total_price,
+    o_orderdate                         AS order_date,
+    o_orderpriority                     AS order_priority,
+    o_shippriority                      AS ship_priority
+FROM {{ source('tpch', 'orders') }}
+
+
