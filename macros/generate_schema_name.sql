4583edae3c3d2a0f3a1c9f5be316681e59453c82
Naim Ares Alonso <naim.alonso@theinformationlab.es>
2026-06-19 14:54:29 +0000

    day5

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,6 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
-        {{ target.schema }}
+        {{ default_schema }}
+    {%- elif env_var('DBT_CLOUD_PR_ID', '') != '' -%}
+        dbt_cloud_pr_{{ env_var('DBT_CLOUD_JOB_ID') }}_{{ env_var('DBT_CLOUD_PR_ID') }}_{{ custom_schema_name | trim }}
     {%- else -%}
         {{ custom_schema_name | trim }}
     {%- endif -%}
